078c474831b12bf40c9358013f0a4f238fb6ee5a
pliablepixels <pliablepixels@gmail.com>
2015-05-20 16:17:14 -0400

    Event notification daemon framework for ZoneMinder

--- zmeventnotification.pl
+++ zmeventnotification.pl
@@ -3,6 +3,7 @@
 # ==========================================================================
 #
 # ZoneMinder Event Watch Script, $Date$, $Revision$
+# ~ asker
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
