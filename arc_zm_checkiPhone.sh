3701c1e9ee32d16f4516fd400803ad9e164fb99e
arjunrc <pliablepixels@gmail.com>
2016-06-08 10:20:37 -0400

    added option to not change states via hard file

--- arc_zm_checkiPhone.sh
+++ arc_zm_checkiPhone.sh
@@ -7,27 +7,47 @@
 # this is mostly the right state to be in (aka your phone = your presence)
    
 
-filepath="/usr/local/share/zoneminder"
+filepath="/usr/share/zoneminder"
 
-zmdbuser=`cat /etc/zm.conf | grep ZM_DB_USER | awk -F= '{print $2}' | tr -d ' '`
-zmdbpass=`cat /etc/zm.conf | grep ZM_DB_PASS | awk -F= '{print $2}' | tr -d ' '`
-
+zmdbuser=`cat /etc/zm/zm.conf | grep ZM_DB_USER | awk -F= '{print $2}' | tr -d ' '`
+zmdbpass=`cat /etc/zm/zm.conf | grep ZM_DB_PASS | awk -F= '{print $2}' | tr -d ' '`
 runstate=`mysql -N -B -u$zmdbuser -p$zmdbpass zm -e "select Name  from States where isActive=1 LIMIT 1;"`
-iphonestate=`cat $filepath/zm_phone_state.txt`
 control="/usr/local/bin/arc_zm_change_state.sh"
-zmlog="/usr/bin/arc_zm_log.pl"
+zmlog="/usr/local/bin/arc_zm_log.pl"
 nowdate=`date`
+file_changestate="zm_change_state.txt"
+file_phonestate="zm_phone_state.txt"
+
+if [ ! -e "$filepath/$file_phonestate" ]; then
+	$zmlog "creating $file_phonestate";
+	echo "out" > "$filepath/$file_phonestate";
+fi
+
+if [ ! -e "$filepath/$file_changestate" ]; then
+	$zmlog "creating $file_changestate";
+	echo "1" > "$filepath/$file_changestate";
+fi
+
+
+iphonestate=`cat $filepath/$file_phonestate`
+switchstate=`cat $filepath/$file_changestate`
+
+if [ "$switchstate" = "0" ]; then
+	$zmlog "ARC: Not switching state, as $filepath/$filechangestate is 0";
+	exit;
+fi
+
 
 
 if [ "$runstate" = "in-modect" ] && [ "$iphonestate" = "out" ]; then
 	$zmlog "ARC:Oops looks like you stepped out and ZM is not aware!"
 	$control out-modect
-	echo "Changed to out-modect at $nowdate - your phone is out" | /usr/bin/mail -s "ZoneMinder:Changed state to out-modect" user@gmail.com	
+	#echo "Changed to out-modect at $nowdate - your phone is out" | /usr/bin/mail -s "ZoneMinder:Changed state to out-modect" arjunrc@gmail.com	
 
 elif [ "$runstate" = "out-modect" ] && [ "$iphonestate" = "in" ]; then
 	$zmlog "Hey, ZM thinks you are out, but your phone says you are in"
 	$control in-modect
-	echo "Changed to in-modect at $nowdate - your phone is in" | /usr/bin/mail -s "ZoneMinder:Changed state to in-modect" user@gmail.com	
+	#echo "Changed to in-modect at $nowdate - your phone is in" | /usr/bin/mail -s "ZoneMinder:Changed state to in-modect" arjunrc@gmail.com	
 
 #else
 	#echo "No need to change state: Your phone says $iphonestate and ZM is in $runstate" >> $filepath/$logfile
